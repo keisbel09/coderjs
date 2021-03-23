5397a17b9781340f9260e31f330d407f5cd777c3
keisbel09 <acostakeisbel@gmail.com>
2021-03-22 21:01:33 -0300

    clase coder js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var myVariable = 1;
+let myVariable2;
+
+const MYCONSTANT = "esto es una constante";
+
+
+var textoA = "keisbel";
+let textoB = "Acosta";
+const espacio = " ";
+
+let resultadoA = textoA + espacio + textoB;
