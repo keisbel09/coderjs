fcd6995c46c372e4c0e400a35092041ebacab602
keisbel09 <acostakeisbel@gmail.com>
2021-03-28 19:37:45 -0300

    agregamos ciclos

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -2,13 +2,19 @@
 let nombreIngresado = prompt ("Nombre");
 let apellidoIngresado = prompt("Apellido");
 
+    
+
 alert("¡Hola! " + nombreIngresado +" " + apellidoIngresado + ", soy Calculator");
+
 
 let diaslaburados = parseInt( prompt( nombreIngresado + ", vamos a calcular tu salario de este mes, ingresa tus días laburados"));
 
-if ((diaslaburados > 0) && (diaslaburados <32 )) {// Verifica que el valor numerico ingresaso sea mayor que 0 y menor que 31
+while ( (diaslaburados <0) || (diaslaburados >32) || (diaslaburados != "")){
+
+if ((diaslaburados > 0) && (diaslaburados <32 )) {// Verifica que el valor numerico ingresaso sea mayor que 0 y menor que 32
     alert ( "Tu salario es de : $" + ( diaslaburados * 1000))
     alert( nombreIngresado + ", espero haber podido ayudarte,  que tengas un excelente día");
+    break;
 }
 
 else if(diaslaburados >31) { // verifica que el valor numerico no sea mayor que 31
@@ -22,4 +28,6 @@ else if(diaslaburados != "") { //verifica que sea ingresado un dato numerico
 else{
     alert("Debes ingresar un numero mayor 0");
 }
+diaslaburados = parseInt( prompt( nombreIngresado + ", vamos a calcular tu salario de este mes, ingresa tus días laburados"));
 
+}
